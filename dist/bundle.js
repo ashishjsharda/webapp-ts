246d168bdfdee0dadcaa83c2eb023cbb8eca4ef2
Ashish Sharda <ashishjsharda@gmail.com>
2022-09-02 07:59:52 -0500

    Add webpack

---
+++ dist/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+console.log('Web App started');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWJhcHAvLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7OztBQUFBIiwiZmlsZSI6ImJ1bmRsZS5qcyIsInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdXZWIgQXBwIHN0YXJ0ZWQnKTtcclxuIl0sInNvdXJjZVJvb3QiOiIifQ==
