36db92e44da0ff1d4fbef64be540e2a7e8888dc5
Ashish Sharda <ashishjsharda@gmail.com>
2022-09-09 21:30:13 -0500

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Web App started');
+import { LocalDataSource } from "./data/localDataSource";
+import { HtmlDisplay } from "./htmlDisplay";
+import "bootstrap/dist/css/bootstrap.css";
+let ds = new LocalDataSource();
+async function displayData(): Promise<HTMLElement> {
+    let display = new HtmlDisplay();
+    display.props = {
+        products: await ds.getProducts("name"),
+        order: ds.order
+    }
+    return display.getContent();
+}
+document.onreadystatechange = () => {
+    if (document.readyState === "complete") {
+        displayData().then(elem => {
+            let rootElement = document.getElementById("app");
+            rootElement.innerHTML = "";
+            rootElement.appendChild(elem);
+        });
+    }
+};
